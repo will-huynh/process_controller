860a526a848845ee618e2a31c1927f6182a37cfa
Coalited <WCoalited@gmail.com>
2018-11-07 19:03:39 -0500

    Changed test function to use division; allows tests for exceptions

--- test_function.py
+++ test_function.py
@@ -4,7 +4,7 @@ import tcp_log_socket
 logging_socket = tcp_log_socket.local_logging_socket(__name__)
 logger = logging_socket.logger
 
-def sum(num1, num2):
-    sum = num1 + num2
-    logger.info("Sum is: {}".format(sum))
-    return sum
+def test(num1, num2):
+    quotient = num1 / num2
+    logger.info("Quotient is: {}".format(quotient))
+    return quotient
